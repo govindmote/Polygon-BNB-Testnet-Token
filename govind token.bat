7fe90fa189f11ebcccf8c54044dc6015a0359aee
govindmote <motegovind75@gmail.com>
2025-08-26 23:08:18 +0530

    Add files via upload

---
+++ govind token.bat
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract govind is ERC20 {
+
+    uint constant _initial_supply =1000 * (10**18);
+
+    constructor() ERC20("govind token", "SDT") {
+        _mint(msg.sender, _initial_supply);
+    }
+}
